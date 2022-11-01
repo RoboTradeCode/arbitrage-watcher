2db7a1022e18de7f6a5ab4decba83b217f5aa253
= <=>
2022-11-01 15:17:54 +0300

    Fix bug 2

--- kernel/start.php
+++ kernel/start.php
@@ -8,8 +8,9 @@ require dirname(__DIR__) . '/index.php';
 $config = require_once dirname(__DIR__) . '/config/arbitrage.config.php';
 
 $symbol = $config['symbol'];
+$not_get_exchanges = $config['not_get_exchanges'];
 
-foreach (Exchange::$exchanges as $exchange) {
+foreach (array_diff(Exchange::$exchanges, $not_get_exchanges) as $exchange) {
     Pm2::start(__DIR__ . '/watch_orderbook.php', 'Orderbook record ' . $symbol . ' ' . $exchange, 'record_orderbook', [$exchange, $symbol]);
     echo '[' . date('Y-m-d H:i:s') . '] Start: ' . $symbol . ' ' . $exchange . PHP_EOL;
 }
