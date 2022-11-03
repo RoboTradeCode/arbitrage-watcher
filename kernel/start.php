b85dfafdbe4d49a81da3846cc0d0316d00f1b50f
= <=>
2022-11-03 18:15:09 +0300

    Update start file

--- kernel/start.php
+++ kernel/start.php
@@ -11,6 +11,9 @@ $symbol = $config['symbol'];
 $not_get_exchanges = $config['not_get_exchanges'];
 
 foreach (array_diff(Exchange::$exchanges, $not_get_exchanges) as $exchange) {
-    Pm2::start(__DIR__ . '/watch_orderbook.php', 'Orderbook record ' . $symbol . ' ' . $exchange, 'record_orderbook', [$exchange, $symbol]);
+    Pm2::start(__DIR__ . '/watch_orderbook.php', 'Orderbook record ' . $symbol . ' ' . $exchange, 'arbitrage_watcher_record_orderbook', [$exchange, $symbol]);
     echo '[' . date('Y-m-d H:i:s') . '] Start: ' . $symbol . ' ' . $exchange . PHP_EOL;
 }
+
+Pm2::start(__DIR__ . '/start.php', 'Orderbook liquidity ' . $symbol, 'arbitrage_watcher_update_liquidity');
+echo '[' . date('Y-m-d H:i:s') . '] Start Update Liquidity' . PHP_EOL;
